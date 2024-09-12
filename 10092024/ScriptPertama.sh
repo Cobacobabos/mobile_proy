1efdcfe241cc8cf5093020af22359af3d4862661
Cobacobabos <tumbalakunnn7@gmail.com>
2024-09-12 11:15:15 +0700

    scriptpertama

---
+++ 10092024/ScriptPertama.sh
@@ -0,0 +1,40 @@
+#echo "siapa nama anda"
+#command ini untuk berinteraksi dengan terminal
+#setelah read merupakan nama variabel yang digunakan untuk menampung jawaban atau interaksi dari user/pengguna terhadap termina
+#read name
+#untuk memanggil variabel yang kita gunakan untuk menampung jawaban atau interaksi gunakan $
+#echo "nama saya adalah $name"
+
+#echo "Tuliskan Panjang = "
+#read Panjang
+
+#echo "Tuliskan Lebar = "
+#read Lebar
+
+#keliling=$((2 * (Panjang + Lebar)))
+#echo "Keliling persegi panjang dengan panjang $Panjang dan lebar $Lebar adalah : $keliling"
+
+#echo "Tulis R = "
+#read r
+#echo "Tulis T = "
+#read t
+#volume=$((314 * r * r * t / 300))
+#echo "volume kerucut dengan jari jari $r dan tinggi $t adalah : $volume"
+#function nm_fungsi(){}
+#nm_fungsi
+
+#function hello () {
+#    echo "dimana tempat tinggal anda
+#}
+#function ayam() {
+ #   echo "Selamat datang"
+#}
+#function HitungGaya () {
+#   echo "Masukan nilai Massa : "
+#   read m
+#   echo "Masukan nilai percepatan : "
+#   read a
+#   echo "Hasil gaya dari massa $m dan percepatan $a adalah $((m*a))"
+#}
+
+#HitungGaya
